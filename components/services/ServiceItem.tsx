2c95cac360e459e814635d2df67f12fddce47a1c
vladlen <vladlengg@gmail.com>
2022-02-13 23:40:42 +0500

    fix eslint

--- components/services/ServiceItem.tsx
+++ components/services/ServiceItem.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Image from "next/image"
 import styles from "./ServiceItem.module.css"
 
 interface ServiceItemProps {
@@ -9,7 +10,7 @@ interface ServiceItemProps {
 const ServiceItem: React.FC<ServiceItemProps> = ({title, icon, children}) => {
     return (
         <div className={styles.item}>
-            <img src={icon} alt={title} className={styles.icon} />
+            <Image src={icon} alt={title} className={styles.icon} width={60} height={40} />
             <div className={styles.title}>{title}</div>
             <div className={styles.desc}>{children}</div>
         </div>
