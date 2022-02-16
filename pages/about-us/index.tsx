af2ce84c5759ed3d404247e073b0ecaede0fbaa3
vladlen <vladlengg@gmail.com>
2022-02-17 01:22:35 +0500

    optimization about us

---
+++ pages/about-us/index.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Header from "../../components/header/Header"
+import Contacts from "../../components/contacts/Contacts"
+import Footer from "../../components/footer/Footer"
+import Banner from "./Banner"
+import Benefits from "./Benefits"
+
+const AboutUs = () => {
+    return (
+        <>
+            <Header />
+            <Banner />
+            <Benefits />
+            <Contacts />
+            <Footer />
+        </>
+    )
+}
+
+export default AboutUs
