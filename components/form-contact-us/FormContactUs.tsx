9dba05dc77e165d895d7fb1827f6e0fc53863ed0
vladlen <vladlengg@gmail.com>
2022-03-07 16:54:55 +0500

    update manifest

--- components/form-contact-us/FormContactUs.tsx
+++ components/form-contact-us/FormContactUs.tsx
@@ -1,23 +1,65 @@
-import React from "react"
+import React, {useRef, useState} from "react"
+import axios from "axios"
 import styles from "./FormContactUs.module.css"
 
 const FormContactUs = () => {
+    const ref = useRef<any>()
+    const [loading, setLoading] = useState(false)
+    const [visible, setVisible] = useState(false)
+    const [visibleMessage, setVisibleMessage] = useState(false)
+    //
+    const serializeForm = () => {
+        const obj = {}
+        const formData = new FormData(ref.current)
+        // @ts-ignore
+        for (const key of formData.keys()) {
+            // @ts-ignore
+            obj[key] = formData.get(key)
+        }
+        return obj
+    }
+    //
+    const onHandler = () => setVisible(prevState => !prevState)
+    //
+    const onSubmitHandler = async (e: any) => {
+        e.preventDefault()
+        setLoading(true)
+        const body = serializeForm()
+        const response = await axios.post("https://api.dtsincorporated.com/dtsbrokers", body)
+        // const response = await axios.post("http://localhost:8000/dts", body)
+        setLoading(false)
+        if (response.status) {
+            setVisible(false)
+            setVisibleMessage(true)
+        }
+    }
+
+    if (visibleMessage)
+        return <>
+            <div className={styles.message}>
+                You have successfully submitted your application. You will be contacted shortly!
+            </div>
+        </>
+
     return (
-        <form className={styles.form}>
-            <select className={styles.input} placeholder="This message is for: " defaultValue="0">
-                <option disabled value="0">This message is for:</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
+        <form className={styles.form} onSubmit={onSubmitHandler} ref={ref}>
+            <select name="for_whom" className={styles.input} placeholder="This message is for: " required>
+                <option value="Sales">This message is for: Sales</option>
+                <option value="Customer">This message is for: Customer</option>
+                <option value="Service">This message is for: Service</option>
+                <option value="Fleet dispatch">This message is for: Fleet dispatch</option>
+                <option value="Careers">This message is for: Careers</option>
             </select>
             <div className={styles.column}>
-                <input className={styles.input} placeholder="Full name" />
-                <input className={styles.input} placeholder="Last name" />
+                <input name="full_name" className={styles.input} placeholder="Full name" required />
+                <input name="last_name" className={styles.input} placeholder="Last name" required />
             </div>
-            <input className={styles.input} placeholder="Email" />
-            <input className={styles.input} placeholder="Phone" />
-            <input className={styles.input} placeholder="Company" />
-            <textarea className={styles.textarea} placeholder="Message" />
-            <button className={styles.button}>Send</button>
+            <input name="email" className={styles.input} placeholder="Email" required />
+            <input name="phone" className={styles.input} placeholder="Phone" required />
+            <input name="company" className={styles.input} placeholder="Company" required />
+            <textarea name="message" className={styles.textarea} placeholder="Message" required />
+            <button type="submit" className={styles.button}
+                    disabled={loading}>{loading ? "Sending..." : "Send"}</button>
         </form>
     )
 }
